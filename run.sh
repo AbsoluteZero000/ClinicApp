f3029cd405a6fbddc29c99ea7d4ec5eb3dc2a9ba
Ahmed Wael Wanas <absolutezero@pop-os.zte.com.cn>
2023-12-01 19:44:03 +0200

    added shell

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
-docker run --name mysql-container --network=mynetwork -e MYSQL_ROOT_PASSWORD=12345678 -d your_mysql_image:latest
+docker build -t backend /Backend/awesomeProject
 
-docker run --name backend-container --network=mynetwork -p 8080:8080 -d backend:latest
+docker build -t database /Database
+
+docker build -t frontend /Frontend/app
+
+
+docker run --name database --network=mynetwork -e MYSQL_ROOT_PASSWORD=12345678 -d database:latest
+sleep 5
+docker run --name backend --network=mynetwork -p 8080:8080 -d backend:latest
 
 docker run --name frontend --network=mynetwork -p 4200:4200 -d frontend:latest
