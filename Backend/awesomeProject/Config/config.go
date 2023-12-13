92fb0a0e0561f50c5df22afac9ca033a3b0b89b9
Ahmed Wael Wanas <ahmedwaelwanas@gmail.com>
2023-12-13 11:49:24 +0200

    Env vars

--- Backend/awesomeProject/Config/config.go
+++ Backend/awesomeProject/Config/config.go
@@ -2,17 +2,23 @@ package Config
 
 import (
 	"database/sql"
+    "os"
+	"fmt"
 )
 
 func Connect() *sql.DB {
-	dbDriver := "mysql"
-	dbUser := "root"
-	dbPass := "12345678"
-	dbName := "ClinicApp"
+	// dbDriver := "mysql"
+	// dbUser := "root"
+	// dbPass := "12345678"
+	// dbName := "ClinicApp"
 
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp(mysql-container:3306)/"+dbName)
-
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp(database:3306)/"+dbName)
 	if err != nil {
+		fmt.Println(err.Error())
 		panic(err.Error())
 	}
 	return db
--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
-docker build -t backend /Backend/awesomeProject
+docker build -t backend ./Backend/awesomeProject
 
-docker build -t database /Database
+docker build -t database ./Database
 
-docker build -t frontend /Frontend/app
+docker build -t frontend ./Frontend/app
 
+docker network create mynetwork
 
-docker run --name database --network=mynetwork -e MYSQL_ROOT_PASSWORD=12345678 -d database:latest
+docker run --name database --network=mynetwork -p 3306:3306 -e MYSQL_ROOT_PASSWORD=12345678 -d database:latest
 sleep 5
-docker run --name backend --network=mynetwork -p 8080:8080 -d backend:latest
+docker run --name backend --network=mynetwork -p 8080:8080"  -d backend:latest
 
 docker run --name frontend --network=mynetwork -p 4200:4200 -d frontend:latest
