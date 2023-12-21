13258475f18b30d58e8b371e1ac4583734c6e50f
Ahmed Wael Wanas <ahmedwaelwanas@gmail.com>
2023-12-21 06:15:41 +0200

    containers 1

--- run.sh
+++ run.sh
@@ -6,8 +6,9 @@ docker build -t frontend ./Frontend/app
 
 docker network create mynetwork
 
-docker run --name database --network=mynetwork -e MYSQL_ROOT_PASSWORD=12345678 -d database:latest
+docker run --name mysql --network=mynetwork -e MYSQL_ROOT_PASSWORD=12345678 -d database:latest
 sleep 5
-docker run --name backend-container -e DB_DRIVER=mysql -e DB_USER=root -e DB_PASS=12345678 -e DB_NAME=ClinicApp -p 8080:8080 --network=mynetwork -d backend:latest
 
-docker run --name frontend -e apiKey=http://localhost:8080  --network=mynetwork -p 4200:4200 -d frontend:latest
+docker run --name backend -e DB_DRIVER=mysql -e DB_USER=root -e DB_PASS=12345678 -e DB_NAME=ClinicApp -p 8080:8080 --network=mynetwork -d backend:latest
+
+docker run --name frontend  --network=mynetwork -p 4200:4200 -d frontend:latest
